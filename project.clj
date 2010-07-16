73cf1d16405c15061463be5769740118b5d835ab
Caspar Florian Ebeling <florian.ebeling@gmail.com>
2010-07-16 15:43:44 +0200

    project.clj: cleanup.

--- project.clj
+++ project.clj
@@ -1,12 +1,8 @@
-(defproject lein-junit "0.0.1-SNAPSHOT"
-  :description "junit plugin for leiningen"
-;;  :namespaces [leiningen.test-java]
+(defproject lein-junit "0.0.2-SNAPSHOT"
+  :description "JUnit plugin for Leiningen"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
 		 [junit/junit "3.8"]
 		 [ant/ant-junit "1.6.5"]
-		 [ant/ant "1.6.5"]
-;;  [leiningen "1.1.0"]
-]
-  :dev-dependencies [])
+		 [ant/ant "1.6.5"]])
 
