845f5e32d2d6c002941881da7411fd5f27641e93
Roman Scherer <roman@burningswell.com>
2010-07-31 19:51:18 +0200

    Cleaned up project.clj.

--- sample/project.clj
+++ sample/project.clj
@@ -6,7 +6,8 @@
 (defproject sample-project "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.2.0-RC1"]]
   :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]
-                     [lein-junit "0.0.3-SNAPSHOT"]]
+                     [lein-junit "0.0.3-SNAPSHOT"]
+                     [junit/junit "4.8.1"]]
   :source-path "src/clojure"
   :java-source-path [["src/java"] ["test/java"]]
   :junit [["classes" :includes "**/*Test.class"]])
