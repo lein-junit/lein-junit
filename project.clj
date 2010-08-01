07bdb0209f1f9cc4b9e5ba9c6e60d8eaba224923
Roman Scherer <roman@burningswell.com>
2010-08-01 09:04:44 +0200

    Using version 1.8.1 of ant-launcher and ant-junit.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lein-junit "0.0.3-SNAPSHOT"
   :description "JUnit plugin for Leiningen"
-  :dependencies [[ant/ant-junit "1.6.5"]
-                 [ant/ant-launcher "1.6.5"]
+  :dependencies [[org.apache.ant/ant-junit "1.8.1"]
+                 [org.apache.ant/ant-launcher "1.8.1"]
                  [org.clojure/clojure "1.2.0-RC1"]
                  [org.clojure/clojure-contrib "1.2.0-RC1"]]
   :dev-dependencies [[leiningen/leiningen "1.2.1-SNAPSHOT"]
