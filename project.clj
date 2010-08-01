4b6766583c36bea5ab97c4402a7f659d92fab459
Roman Scherer <roman@burningswell.com>
2010-08-01 10:14:29 +0200

    Removed ant-launcher dependency.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject lein-junit "0.0.3-SNAPSHOT"
   :description "JUnit plugin for Leiningen"
   :dependencies [[org.apache.ant/ant-junit "1.8.1"]
-                 [org.apache.ant/ant-launcher "1.8.1"]
                  [org.clojure/clojure "1.2.0-RC1"]
                  [org.clojure/clojure-contrib "1.2.0-RC1"]]
   :dev-dependencies [[leiningen/leiningen "1.2.1-SNAPSHOT"]
+                     [lein-javac "1.2.1-SNAPSHOT"]
                      [swank-clojure "1.2.1"]])
 
