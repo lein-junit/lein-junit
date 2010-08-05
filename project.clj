84661b59ed7d513d6c26a53930a444725fc7c058
Roman Scherer <roman.scherer@nugg.ad>
2010-08-05 16:15:43 +0200

    Added authors and github url.

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject lein-junit "0.0.3-SNAPSHOT"
+  :author "Caspar Florian Ebeling, Roman Scherer"
   :description "JUnit plugin for Leiningen"
+  :url "http://github.com/febeling/lein-junit"
   :dependencies [[ant/ant-junit "1.6.5"]
                  [org.clojure/clojure "1.2.0-RC1"]
                  [org.clojure/clojure-contrib "1.2.0-RC1"]]
