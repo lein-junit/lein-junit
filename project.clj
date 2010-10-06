c98c9e7fe6a1cffae8a97f57dd2e91fd2026e60b
Roman Scherer <roman.scherer@nugg.ad>
2010-10-06 15:52:21 +0200

    Upgraded to clojure 1.2.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :description "JUnit plugin for Leiningen"
   :url "http://github.com/febeling/lein-junit"
   :dependencies [[ant/ant-junit "1.6.5"]
-                 [org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
+                 [org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[leiningen/leiningen "1.3.0-SNAPSHOT"]
                      [lein-javac "1.2.1-SNAPSHOT"]
                      [swank-clojure "1.2.1"]])
