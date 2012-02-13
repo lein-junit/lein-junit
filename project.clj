cb7fe3a3c2b48a3b0f2216426a43022a2ccd79ec
Roman Scherer <roman.scherer@nugg.ad>
2012-02-13 14:24:39 +0100

    Started 0.0.6-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-junit "0.0.5"
+(defproject lein-junit "0.0.6-SNAPSHOT"
   :author "Caspar Florian Ebeling, Roman Scherer"
   :description "JUnit plugin for Leiningen"
   :url "http://github.com/febeling/lein-junit"
