c049620e3e8c84b4d175ac7c09c874b6df44ab99
Roman Scherer <roman.scherer@nugg.ad>
2012-02-14 17:51:27 +0100

    Depend on Leiningen 1.6.2.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-junit "0.0.7-SNAPSHOT"
+(defproject lein-junit "0.0.8-SNAPSHOT"
   :author "Caspar Florian Ebeling, Roman Scherer"
   :description "JUnit plugin for Leiningen"
   :url "http://github.com/febeling/lein-junit"
   :dependencies [[ant/ant-junit "1.6.5"]]
-  :dev-dependencies [[leiningen/leiningen "1.6.2-SNAPSHOT"]
+  :dev-dependencies [[leiningen/leiningen "1.6.2"]
                      [junit/junit "4.8.1"]])
