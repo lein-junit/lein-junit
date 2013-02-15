d636982de578cd2378eaec77c4800a33ba1c3d85
Derek Dagit <derekd@yahoo-inc.com>
2013-02-15 10:15:06 -0600

    Pull in newer ant-juint 1.8.4 to run JUnit4 tests

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ant/ant-junit "1.6.5"]
+  :dependencies [[org.apache.ant/ant-junit "1.8.4"]
                  [lancet "1.0.1"]
                  [junit/junit "4.10"]]
   :eval-in-leiningen true)
