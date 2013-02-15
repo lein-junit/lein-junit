184a3384ea344f71a7137ff65c0c024f54da36a5
Roman Scherer <roman.scherer@nugg.ad>
2013-02-15 17:29:22 +0100

    Release 1.1.2.

--- sample/project.clj
+++ sample/project.clj
@@ -2,7 +2,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[junit/junit "4.11"]]}}
-  :plugins [[lein-junit "1.1.1-SNAPSHOT"]]
+  :plugins [[lein-junit "1.1.2"]]
   :hooks []
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java" "test/java"]
