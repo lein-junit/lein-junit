bcb1da671f560b6299141d46254d0c7b9b8b0c9e
Roman Scherer <roman.scherer@nugg.ad>
2013-07-31 11:58:48 +0200

    Use lein-junit 1.1.3 in sample project.

--- sample/project.clj
+++ sample/project.clj
@@ -2,7 +2,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.0"]]
   :profiles {:dev {:dependencies [[junit/junit "4.11"]]}}
-  :plugins [[lein-junit "1.1.2"]]
+  :plugins [[lein-junit "1.1.3"]]
   :hooks []
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java" "test/java"]
