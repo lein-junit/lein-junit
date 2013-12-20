2574ff7fb4a5be71047c540b3d2c25ab2298878a
Roman Scherer <roman.scherer@burningswell.com>
2013-12-20 20:27:47 +0100

    lein-release plugin: bumped version from 1.1.5 to 1.1.6-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-junit "1.1.5"
+(defproject lein-junit "1.1.6-SNAPSHOT"
   :author "Caspar Florian Ebeling, Roman Scherer"
   :description "JUnit plugin for Leiningen"
   :url "http://github.com/febeling/lein-junit"
