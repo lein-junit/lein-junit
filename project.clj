cf4371c19fbe2775026c2f47e97ae7a1f9b17e33
Roman Scherer <roman.scherer@burningswell.com>
2014-04-14 18:57:21 +0200

    lein-release plugin: preparing 1.1.6 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-junit "1.1.6-SNAPSHOT"
+(defproject lein-junit "1.1.6"
   :author "Caspar Florian Ebeling, Roman Scherer"
   :description "JUnit plugin for Leiningen"
   :url "http://github.com/febeling/lein-junit"
