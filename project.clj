032854cdd55857b077c74ac4882960d3d5374ad4
Roman Scherer <roman@burningswell.com>
2014-07-15 22:45:24 +0200

    Replace :lein-release with :deploy-repositories

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :description "JUnit plugin for Leiningen"
   :url "http://github.com/febeling/lein-junit"
   :min-lein-version "2.0.0"
-  :lein-release {:deploy-via :clojars}
+  :deploy-repositories [["releases" :clojars]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.apache.ant/ant-junit "1.8.4"]
