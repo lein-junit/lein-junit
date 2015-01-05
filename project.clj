073d1771831ba2de67010e8c773473903c7c466c
Florian Ebeling <florian.ebeling@gmail.com>
2015-01-05 13:36:27 +0100

    Version 1.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-junit "1.1.8-SNAPSHOT"
+(defproject lein-junit "1.1.8"
   :author "Caspar Florian Ebeling, Roman Scherer"
   :description "JUnit plugin for Leiningen"
   :url "http://github.com/febeling/lein-junit"
