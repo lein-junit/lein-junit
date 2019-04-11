464d47d3f66b8a0c0d3147ca0a50a2717077dc37
r0man <roman@burningswell.com>
2019-04-11 21:47:32 +0200

    Version 1.1.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-junit "1.1.9"
+(defproject lein-junit "1.1.10-SNAPSHOT"
   :author "Caspar Florian Ebeling, Roman Scherer"
   :description "JUnit plugin for Leiningen"
   :url "http://github.com/febeling/lein-junit"
